2edc974e1e66a1f96f8faf602f37a9036a15d648
Ryan Krafnick <kraflab@gmail.com>
2017-04-13 20:28:22 -0400

    Finalized format

--- heretic-n-parse.rb
+++ heretic-n-parse.rb
@@ -1,13 +1,14 @@
+# List of category file keys 'h1x1-yyy'
 category_keys = {
-  'm' => '"SM Max"',
-  'f' => '"BP Max"',
-  'x' => '"NM Max"',
-  'n' => '"NM Speed"',
-  'o' => '"NoMo"',
-  'p' => '"Pacifist"',
-  's' => '"SM Speed"',
-  'b' => '"BP Speed"',
-  't' => '"Tyson"'
+  'm' => 'SM Max',
+  'f' => 'BP Max',
+  'x' => 'NM Max',
+  'n' => 'NM Speed',
+  'o' => 'NoMo',
+  'p' => 'Pacifist',
+  's' => 'SM Speed',
+  'b' => 'BP Speed',
+  't' => 'Tyson'
 }
 
 # Given a list of files, extract author information, and parse file name
@@ -18,16 +19,22 @@ files.each do |file_name|
   short_name = base_name.downcase
   level = short_name[0..3]
   category = category_keys[level[2]]
-  level[0] = 'e'
-  level[2] = 'm'
+  level[0] = 'E'
+  level[2] = 'M'
   time_chars = short_name[4..-1].chars
-  time_chars.shift while time_chars[0] =~ /[-0]/
+  time_chars.shift while time_chars[0] == '-'
   level += time_chars.shift if time_chars[0] == 's'
   time = time_chars.join
-  time.insert(-3, ':') if time.length > 2
+  time.insert(-3, ':')
   success = false
-  File.readlines(file_name).each do |line|
-    if line[0..5].downcase == 'author'
+  result = {tas: 0, guys: 1, recorded_at: nil, file: nil, version: 0,
+            wad_username: 'heretic', time: time, level: level,
+            levelstat: time, category_name: category, video_link: nil}
+  file = File.open(file_name)
+  file.readlines.each do |line|
+    case line[0..3].downcase
+    when 'auth'
+      line = line.force_encoding('UTF-8')
       text = line.gsub(/\[.+\]|\(.+\)|<.+>|".+"|:|--/, '').scan /[^\s]+/
       text.shift
       text.reject! { |i| i =~ /@/ }
@@ -38,10 +45,22 @@ files.each do |file_name|
       when '-=QWERTY=-'
         name = 'QWERTY'
       end
-      puts "#{level} #{category} #{time} \"#{name}\" #{base_name}.zip"
+      result[:players] = name
       success = true
-      break
+    when 'game'
+      # Check for vv's hack
+      result[:engine] = line =~ /vv/ ? 'Heretic v1.3 + vv' : 'Heretic v1.3'
     end
   end
-  STDERR.puts "Fail: #{short_name}!" unless success
+  if success
+    result[:engine] ||= 'Heretic v1.3'
+    # Syntax for use with the api
+    print 'post demo ='
+    result.each do |k, v|
+      print " #{k}: \"#{v}\""
+    end
+    puts
+  else
+    STDERR.puts "Fail: #{base_name}!"
+  end
 end
